ecafc129fc411eba21ffaec6b5244a79440b87a9
Jeroen de Bruijn <vidavidorra@gmail.com>
2016-02-21 11:55:09 +0100

    Add Travis, convert script, license and readme

---
+++ README.md
@@ -0,0 +1,68 @@
+# Convert Atmel Studio Makefile to Unix format
+[![License GPLv3][badge-license]][license]
+[![Travis build status][badge-travis]][travis-build]
+
+Example of an Atmel Studio project for an ATmega328P with automated build on [Travis CI][travis-ci] or any other Linux-based system.
+
+
+# Documentation
+At the moment this script is only tested using an Atmel Studio 7 project for an ATmega328P, but it should work for every MCU. Please let me know if you've tested it with a different version of Atmel Studio or a different MCU.
+
+## Set up repository for Travis CI
+In order to set up a repository of a Atmel Studio project for automatic builds on Travis CI the `.travis.yml` and `convertASMakefileToUnix.sh` scripts need to be present in the repository. Examples of those scripts can be found in this repository. Once those files are present in the repository, Travis CI needs to know that it has to build a project. More information on how to setup a repository with Travis CI can be found in their [Getting Started Docs][travis-getstarted].
+
+## Atmel AVR Toolchain for Linux
+In order to be able to build an Atmel Studio project on Linux, the [Atmel AVR Toolchain for Linux][atmel-toolchain] needs to be present on the system.
+Linux provides these packages and they can be installed as shown below.
+```shell
+sudo apt-get update
+sudo apt-get install binutils-avr gcc-avr avr-libc
+```
+
+## convertASMakefileToUnix script
+This script converts a Atmel Studio, and thus Windows, makefile to a Linux makefile. It requires the Atmel AVR Toolchain for Linux to be installed on the system.
+
+### Usage
+```
+usage: convertASMakefileToUnix.sh [-h] PROJECT_DIR
+
+This script converts an Atmel Studio 7 makefile to a Unix format.
+This way it should be possible to build the project under Unix (for example Ubuntu) using the Atmel AVR Toolchain for Linux.
+
+positional arguments:
+  PROJECT_DIR        The directory of the project.
+
+optional arguments:
+  -h,                Show this help message and exit.
+```
+
+### Example
+```
+lizzard@ubuntu:~/Documents/AtmelStudio_Travis-CI_Example$ ./convertMkToUnix.sh AtmelStudio_Travis-CI_Example
+convertASMakefileToUnix 0.1.0 Copyright (c) 2016 Jeroen de Bruijn <vidavidorra@gmail.com>
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it under certain conditions.
+
+TARGET_DIR: AtmelStudio_Travis-CI_Example/Debug
+TARGET_MAKEFILE: AtmelStudio_Travis-CI_Example/Debug/Makefile
+AVR_GCC_VERSION: 4.5.3
+
+*** convertASMakefileToUnix has finished ***
+```
+
+# Licensing
+GNU General Public License version 3 or later, as published by the Free Software Foundation.
+Modification and redistribution are permitted according to the terms of the GPL.
+The license can be found in the `LICENSE` file.
+
+[Pull requests](https://github.com/vidavidorra/AtmelStudio_Travis-CI_Example/pulls) and [bug reports](https://github.com/vidavidorra/AtmelStudio_Travis-CI_Example/issues) are welcome!
+
+
+[badge-license]:        https://img.shields.io/badge/license-GPLv3-blue.svg
+[license]:              https://github.com/vidavidorra/AtmelStudio_Travis-CI_Example/blob/master/LICENSE
+[badge-travis]:         https://travis-ci.org/vidavidorra/BitOperations.svg
+[travis-build]:         https://travis-ci.org/vidavidorra/BitOperations
+[travis-ci]:            https://travis-ci.org
+[travis-getstarted]:    https://docs.travis-ci.com/user/getting-started
+[travis-docs]:          https://docs.travis-ci.com
+[atmel-toolchain]:      http://www.atmel.com/tools/atmelavrtoolchainforlinux.aspx
---
+++ convertASMakefileToUnix.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+#~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+# 
+# Copyright (c) 2016  Jeroen de Bruijn  <vidavidorra@gmail.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+#title           : convertASMakefileToUnix.sh
+#date created    : 2016/02/20
+#notes           :
+__AUTHOR__="Jeroen de Bruijn"
+__LICENSE__="GPLv3"
+__VERSION__="0.1.0"
+__MAINTAINER__="Jeroen de Bruijn"
+__EMAIL__="vidavidorra@gmail.com"
+__LICENSE_INFO__="convertASMakefileToUnix ${__VERSION__}  Copyright (c) 2016  "
+__LICENSE_INFO__=$__LICENSE_INFO__"${__AUTHOR__} <${__EMAIL__}>\n"
+__LICENSE_INFO__=$__LICENSE_INFO__"This program comes with ABSOLUTELY NO WARRANTY.\n"
+__LICENSE_INFO__=$__LICENSE_INFO__"This is free software, and you are welcome to "
+__LICENSE_INFO__=$__LICENSE_INFO__"redistribute it under certain conditions."
+__DESCRIPTION__="This script converts an Atmel Studio 7 makefile to a Unix format.\n"
+__DESCRIPTION__=$__DESCRIPTION__"This way it should be possible to build the project "
+__DESCRIPTION__=$__DESCRIPTION__"under Unix (for example Ubuntu) using the Atmel AVR "
+__DESCRIPTION__=$__DESCRIPTION__"Toolchain for Linux."
+#
+# This script converts an Atmel Studio 7 makefile to a Unix format.
+# This way it should be possible to build the project under Unix
+# (for example Ubuntu) using the Atmel AVR Toolchain for Linux.
+#
+#~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*
+
+PROJECT_DIR=$1
+
+echo $__LICENSE_INFO__
+echo ''
+
+# Check if avr-gcc is installed
+if ! type "avr-gcc" > /dev/null; then
+    echo 'Error: The program "avr-gcc" is not installed on this system.'
+    echo 'Please install and run this script again.'
+    echo ' Install suggestion: "sudo apt-get install avr-gcc"'
+    echo 'exiting...'
+    exit 1
+fi
+
+# Check if an correct argument is passed and show help message accordingly.
+if [ "$PROJECT_DIR" = "-h" ] || [ "$PROJECT_DIR" = "" ] || [ ! -d "$PROJECT_DIR" ]; then
+    echo 'usage: convertASMakefileToUnix.sh [-h] PROJECT_DIR'
+    echo ''
+    echo $__DESCRIPTION__
+    echo ''
+    echo 'positional arguments:'
+    echo '  PROJECT_DIR        The directory of the project.'
+    echo ''
+    echo 'optional arguments:'
+    echo '-h, --help         show this help message and exit'
+    
+    # If the project directory does not exist show a error message.
+    if [ ! -d "$PROJECT_DIR" ] && [ "$PROJECT_DIR" != "-h" ] && [ "$PROJECT_DIR" != "" ]; then
+        echo ''
+        echo "Error: Directory \"$(pwd)/${PROJECT_DIR}\" does not exist."
+        echo 'exiting...'
+    fi
+    exit 1
+else
+    TARGET_DIR=${PROJECT_DIR}/Debug
+    TARGET_MAKEFILE=${TARGET_DIR}/Makefile
+    AVR_GCC_VERSION=$(avr-gcc --version | grep ^avr-gcc | sed 's/^.* //g')
+    
+    echo "TARGET_DIR: ${TARGET_DIR}"
+    echo "TARGET_MAKEFILE: ${TARGET_MAKEFILE}"
+    echo "AVR_GCC_VERSION: ${AVR_GCC_VERSION}"
+    
+    sed -i 's/cmd.exe/sh/g' ${TARGET_MAKEFILE}
+    sed -i "s/4.9.2/${AVR_GCC_VERSION}/g" ${TARGET_MAKEFILE}
+    sed -i 's/C:\\Program Files (x86)\\Atmel\\Studio\\7.0\\toolchain\\avr8\\avr8-gnu-toolchain\\bin\\avr-/\/usr\/bin\/avr-/g' ${TARGET_MAKEFILE}
+    sed -i 's/.exe//g' ${TARGET_MAKEFILE}
+    echo ''
+    echo '*** convertASMakefileToUnix has finished ***'
+    exit 0
+fi
